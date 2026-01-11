3d40394a101ecd1776485823cf41fa2819f5065d
Thomas Wang <github@paced.me>
2026-01-12 03:29:36 +1100

    Fix various problems with the app and implement more features before 1.0

--- apps/swiper/pages/index/+Page.tsx
+++ apps/swiper/pages/index/+Page.tsx
@@ -17,21 +17,35 @@ import {
   Title,
 } from "@mantine/core";
 import { useLocalStorage } from "@mantine/hooks";
-import { useCallback, useEffect, useState } from "react";
+import { formatDistance } from "date-fns";
+import {ChangeEvent, useCallback, useEffect, useState} from "react";
+import { compactInteger } from "humanize-plus";
 
 const TAGS_TO_REMOVE = [
-  "Custom Volume Controls",
-  "Keyboard Only Option",
-  "Stereo Sound",
-  "Valve Anti-Cheat enabled",
-  "Remote Play Together",
-  "Family Sharing",
-  "Steam Achievements",
-  "Steam Cloud",
-  "Steam Leaderboards",
-  "Steam Trading Cards",
-  "Partial controller support",
-  "Full controller support",
+  "custom volume controls",
+  "keyboard only option",
+  "stereo sound",
+  "surround sound",
+  "valve anti-cheat enabled",
+  "remote play together",
+  "family sharing",
+  "steam achievements",
+  "steam cloud",
+  "steam leaderboards",
+  "steam trading cards",
+  "partial controller support",
+  "full controller support",
+  "remote play on phone",
+  "remote play on tablet",
+  "touch only option",
+  "mouse only option",
+  "adjustable difficulty",
+  "captions available",
+  "adjustable text size",
+  "stats",
+  "color alternatives",
+  "camera comfort",
+  "remote play on tv",
 ];
 
 export default function Home() {
@@ -43,14 +57,14 @@ export default function Home() {
   const [isDirectusFormShown, setIsDirectusFormShown] = useState<boolean>(true);
 
   const handleDirectusUrlChange = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
+    (e: ChangeEvent<HTMLInputElement>) => {
       setDirectusUrl(e.currentTarget.value);
     },
     [setDirectusUrl],
   );
 
   const handleDirectusTokenChange = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
+    (e: ChangeEvent<HTMLInputElement>) => {
       setDirectusToken(e.currentTarget.value);
     },
     [setDirectusToken],
@@ -155,7 +169,7 @@ export default function Home() {
 
   const steamReviewsText =
     currentGame?.Steam_Positive_Reviews && currentGame?.Steam_Negative_Reviews
-      ? `(${currentGame.Review_Category}) ( ${Math.round((100.0 * currentGame.Steam_Positive_Reviews) / currentGame.Steam_Total_Reviews)}%)`
+      ? currentGame.Review_Category
       : "N/A";
 
   let metacriticColor = "red";
@@ -171,9 +185,25 @@ export default function Home() {
     metacriticColor = "darkgrey";
   }
 
+  let steamSentimentColor = "red";
+  if (steamReviewsText === "Mixed") {
+    steamSentimentColor = "orange";
+  } else if (steamReviewsText === "Mostly Positive" || steamReviewsText === "Positive") {
+    steamSentimentColor = "green";
+  } else if (steamReviewsText === "Very Positive" || steamReviewsText === "Overwhelmingly Positive") {
+    steamSentimentColor = "teal";
+  } else if (!steamReviewsText) {
+    steamSentimentColor = "darkgrey";
+  }
+
+  // TODO: 1) images do not load instantly when moving from page to page, 2) inconsistent heights of carousel.
+
   return (
     <Container>
-      <Box mt="xl">
+      <Text mt="md" size="xs" style={{ textAlign: "center"}}>
+        Game {inMemoryGameItemIndex + 1} of {inMemoryGameItems.length} in queue...
+      </Text>
+      <Box mt="xs">
         <Carousel
           slideSize="55%"
           slideGap="sm"
@@ -185,26 +215,26 @@ export default function Home() {
         >
           {(currentGame.Screenshots?.split("\n") || []).map((screenshotUrl: string, index: number) => (
             <CarouselSlide key={index}>
-              <Image src={screenshotUrl} style={{ maxHeight: "384px", objectFit: "contain" }} />
+              <Image src={screenshotUrl} style={{ height: "384px", objectFit: "contain" }} />
             </CarouselSlide>
           ))}
         </Carousel>
       </Box>
-      <Grid mt="md">
+      <Title mt="md">{currentGame?.Name}</Title>
+      <Text size="xs">
+        by {currentGame?.Developers.join(", ")}, release year:{" "}
+        {currentGame?.Release_Date ? new Date(currentGame.Release_Date).getFullYear() : "N/A"}
+      </Text>
+      <Grid>
         <GridCol span={{ base: 12, md: 6 }} h="100%">
-          <Title>{currentGame?.Name}</Title>
-          <Text size="xs" mb="xs">
-            by {currentGame?.Developers.join(", ")}, release year:{" "}
-            {currentGame?.Release_Date ? new Date(currentGame.Release_Date).getFullYear() : "N/A"}
-          </Text>
           <Box my="md" style={{ textAlign: "center" }}>
-            {currentGame?.Tags.filter((tag: any) => !TAGS_TO_REMOVE.includes(tag)).map((tag: string) => (
+            {([...(currentGame?.Spy_Tags || []), ...(currentGame?.Tags || [])]).filter((tag: any) => !TAGS_TO_REMOVE.includes(tag.toLowerCase())).map((tag: string) => (
               <Badge key={`tag-${tag}`} mr="xs" mb="xs" size="xs">
                 {tag}
               </Badge>
             ))}
           </Box>
-          <Stack gap="4px">
+          <Flex direction="row" align="center" justify="center" gap="xl" my="md">
             <Anchor href={currentGame?.URL} target="_blank" rel="noopener noreferrer" size="sm">
               View on {currentGame.Marketplace} Store
             </Anchor>
@@ -221,62 +251,77 @@ export default function Home() {
             >
               View on Directus
             </Anchor>
-          </Stack>
+          </Flex>
         </GridCol>
         <GridCol span={{ base: 12, md: 6 }} style={{ alignSelf: "center" }}>
           <Text size="sm" my="auto">
-            <i>{currentGame?.Description}</i>
+            <i>{currentGame?.Description.replace("&amp;", "&")}</i>
           </Text>
         </GridCol>
       </Grid>
+
       <Grid my="md">
-        <GridCol span={{ base: 12, md: 4 }}>
+        <GridCol span={{ base: 12, md: 6 }}>
           <Flex direction="row" align="center" justify="center">
             <Flex direction="column" align="center">
-              <Title order={4} c={metacriticColor}>
+              <Title order={6} c={metacriticColor}>
                 {currentGame?.Metacritic_Score || "N/A"}
               </Title>
               <Text size="xs">Metacritic</Text>
             </Flex>
             <Flex direction="column" ml="lg" align="center">
-              <Title order={4}>{currentGame?.Steam_Total_Reviews || "N/A"}</Title>
+              <Title order={6}>{currentGame?.Steam_Total_Reviews ? compactInteger(currentGame?.Steam_Total_Reviews, 1) : "N/A"}</Title>
               <Text size="xs">Steam Reviews</Text>
             </Flex>
             <Flex direction="column" ml="lg" align="center">
-              <Title order={4}>{steamReviewsText}</Title>
+              <Title order={6} c={steamSentimentColor}>{steamReviewsText}</Title>
               <Text size="xs">Steam Sentiment</Text>
             </Flex>
           </Flex>
         </GridCol>
-        <GridCol span={{ base: 12, md: 4 }}>
+        <GridCol span={{ base: 12, md: 6 }}>
           <Flex direction="row" align="center" justify="center" mx="auto">
             <Flex direction="column" align="center">
-              <Title order={4}>{currentGame?.HLTB_Hours || "N/A"}</Title>
+              <Title order={6}>{currentGame?.HLTB_Hours || "N/A"}</Title>
               <Text size="xs">HLTB Main</Text>
             </Flex>
             <Flex direction="column" ml="lg" align="center">
-              <Title order={4}>{currentGame?.HLTB_Extra || "N/A"}</Title>
+              <Title order={6}>{currentGame?.HLTB_Extra || "N/A"}</Title>
               <Text size="xs">HLTB Extra</Text>
             </Flex>
             <Flex direction="column" ml="lg" align="center">
-              <Title order={4}>{currentGame?.HLTB_Completionist || "N/A"}</Title>
+              <Title order={6}>{currentGame?.HLTB_Completionist || "N/A"}</Title>
               <Text size="xs">HLTB Comp.</Text>
             </Flex>
-          </Flex>
-        </GridCol>
-        <GridCol span={{ base: 12, md: 4 }}>
-          <Flex direction="row" align="center" justify="center" mx="auto">
             <Flex direction="column" ml="lg" align="center">
-              <Title order={4}>{currentGame?.Hours || "N/A"}</Title>
+              <Title order={6}>{currentGame?.Hours || "N/A"}</Title>
               <Text size="xs">Your Hours</Text>
-            </Flex>
-            <Flex direction="column" ml="lg" align="center">
-              <Title order={4}>{currentGame?.Last_Played?.toLocaleString() || "N/A"}</Title>
-              <Text size="xs">Last Played</Text>
             </Flex>
           </Flex>
         </GridCol>
       </Grid>
+      <Box>
+        <Button mr="sm"
+          disabled={inMemoryGameItemIndex <= 0}
+          onClick={() => {
+            if (inMemoryGameItemIndex > 0) {
+              setInMemoryGameItemIndex(inMemoryGameItemIndex - 1);
+            }
+          }}
+          >
+          Previous
+        </Button>
+        <Button
+          disabled={inMemoryGameItemIndex >= inMemoryGameItems.length - 1}
+          onClick={() => {
+            if (inMemoryGameItemIndex < inMemoryGameItems.length - 1) {
+              setInMemoryGameItemIndex(inMemoryGameItemIndex + 1);
+            }
+          }}
+          >
+          Next
+        </Button>
+      </Box>
     </Container>
   );
 }
